78d7e926619db5cee7e22ec3d847d822592224db
cesar-augusto-ferreira <cesaraugustoferreira@hotmail.com>
2022-10-18 20:45:50 -0300

    inicio

--- app_ciclo_cell/lib/main.dart
+++ app_ciclo_cell/lib/main.dart
@@ -1,15 +1,16 @@
 
 // ignore_for_file: prefer_const_constructors
 
-import 'package:app_ciclo_cell/tela_login.dart';
+
+import 'package:app_ciclo_cell/view/tela_login.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   
   runApp(
     MaterialApp(
-      title: 'Cico Cell',
-      home:  TelaLogin(),
+      title: 'CicloCell',
+      home: TelaLogin(),
       debugShowCheckedModeBanner: false,
     ),
     );
